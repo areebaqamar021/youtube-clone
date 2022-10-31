1279cf35c68367aa4be9cdd74fe0706ebfd419c4
areebaqamar021 <areebaqamar021@gmail.com>
2022-10-31 08:48:44 -0700

    new

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
 
 const App = () => {
-  return (
-    <div>
-      App
-    </div>
-  )
+  <BrowserRouter>
+    <Box sx={{ background: '#000'}}>
+        Navbar
+        <Routes>
+          <Route path="/" exact element={<feed />} />
+        </Routes>
+    </Box>
+  </BrowserRouter>
 }
 
 export default App
